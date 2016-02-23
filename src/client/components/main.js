6afb3a63bd1b0b10d42a7a3c50403dd95eb98b6f
Scott Davis <scott@thirstyhead.com>
2016-02-23 15:19:19 -0700

    stubbed out working Hello World example in Angular2

---
+++ src/client/components/main.js
@@ -0,0 +1,6 @@
+import 'zone.js/dist/zone.min.js';
+import 'reflect-metadata';
+import 'core-js';
+import {bootstrap} from 'angular2/platform/browser';
+import {App} from './app/app';
+bootstrap(App);
