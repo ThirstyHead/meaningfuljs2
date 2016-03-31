9fe16894900fab54eaab0f598b5ffa3cc1df263e
Scott Davis <scott@thirstyhead.com>
2016-03-31 09:29:14 -0600

    stubbed out books.service

--- src/client/components/books/books.component.js
+++ src/client/components/books/books.component.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import {Component} from 'angular2/core';
+import {BooksService} from './books.service';
 
 @Component({
   selector: 'my-books',
@@ -10,5 +11,30 @@ import {Component} from 'angular2/core';
 export class BooksComponent{
   constructor(){
     this.title = "Books";
+    // this.booksService = booksService;
+    this.bookList = [];
   }
+
+  // Angular 2 Dependency Injection for ECMAScript 6
+  // If you're using TypeScript, you can use @Inject on
+  // constructor parameters. Sadly, this is not valid
+  // ES6 or ES7 syntax.
+  // NOTE: 1st in call order
+  static get parameters(){
+    // return [[BooksService]];
+  }
+
+  ngOnInit(){
+    // this.getBooks();
+    console.log(this.bookList);
+  }
+
+  getBooks(){
+    // this.booksService.getList()
+    //                  .subscribe(
+    //                    books => this.bookList = books,
+    //                    error => this.errorMessage = error
+    //                  )
+  }
+
 }
---
+++ src/client/components/books/books.service.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import {Injectable} from 'angular2/core';
+import {Http, Response} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class BooksService{
+  constructor(http){
+    this.http = http;
+    this.url = '/mock/books';
+  }
+
+  static get parameters(){
+    return [[Http]];
+  }
+
+  ngOnInit(){
+
+  }
+
+  getList(){
+    return this.http.get(this.url)
+                    .map( (res) => {
+                      return res.json();
+                    })
+                    .catch(this.handleError);
+  }
+
+  handleError(err){
+    console.error(err);
+    return Observable.throw(error.json().error || 'Error in BooksService');
+  }
+}
